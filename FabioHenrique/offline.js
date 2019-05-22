5d19da94af85c5946ec344f1dbabcc92ecade2c5
fabiohe <50410894+fabiohe@users.noreply.github.com>
2019-05-22 10:15:33 -0300

    Add files via upload

---
+++ FabioHenrique/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1558529515,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/astronave-sheet0.png",
+		"images/botãotiro-sheet0.png",
+		"images/botãotiro-sheet1.png",
+		"images/controletiro-sheet0.png",
+		"images/setasobe-sheet0.png",
+		"images/setasobe-sheet1.png",
+		"images/setadireita-sheet0.png",
+		"images/setadireita-sheet1.png",
+		"images/setaesquerda-sheet0.png",
+		"images/setaesquerda-sheet1.png",
+		"images/setadesce-sheet0.png",
+		"images/setadesce-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/objetoinimigo-sheet0.png",
+		"images/botãodestruir-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
